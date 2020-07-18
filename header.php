e03222b0f7906b5aecc563ad71692c7d386e0a9b
foo <foo@bar.de>
2020-07-18 17:14:21 +0200

    fix: fix typo

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" type="image/x-icon" />
 		<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon-32x32.png" sizes="32x32" />
 		<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon-16x16.png" sizes="16x16" />
-css
 		<meta name="viewport" content="width=device-width">
 
 		<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.css">
